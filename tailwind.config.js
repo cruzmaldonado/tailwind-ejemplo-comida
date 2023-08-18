14d077b941a14ae80496a2ebe8fb999622539881
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-08-18 16:16:59 -0400

    primer proyecto de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#f3c615',
+        'secondary':'#353535',
+
+      },
+      height:{
+        '80':'20rem',
+        '100':'25rem'
+
+      }
+    },
+  },
+  variant:{
+    opacity:['responsive','hover','focus','disble']
+  },
+  plugins: [],
+}
